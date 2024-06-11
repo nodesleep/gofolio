1e76413a0126592d63b2b6efa1b764b35a7dd3df
Aron Sweet <sweet@andrews.edu>
2024-06-11 16:39:36 -0500

    GO GO GO GO GO GO GO

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"html/template"
+	"io"
+
+	"crypto/rand"
+	"math/big"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+type Product struct {
+	Key string
+}
+
+type KeyConfig struct {
+	KeyLength  int
+	GroupCount int
+	Charset    string
+}
+
+func generateProductKey(config KeyConfig) string {
+	key := ""
+	for i := 0; i < config.GroupCount; i++ {
+		if i > 0 {
+			key += "-"
+		}
+		key += generateRandomString(config.KeyLength, config.Charset)
+	}
+	return key
+}
+
+func generateRandomString(length int, charset string) string {
+	result := make([]byte, length)
+	for i := range result {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		if err != nil {
+			panic(err)
+		}
+		result[i] = charset[num.Int64()]
+	}
+	return string(result)
+}
+
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplate() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+func main() {
+	config := KeyConfig{
+		KeyLength:  5,
+		GroupCount: 5,
+		Charset:    "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+	}
+
+	
+	e := echo.New()
+	e.Use(middleware.Logger())
+	
+	htmx := Product{Key: "0"}
+
+	e.Renderer = newTemplate()
+	
+	e.Static("/static", "static")
+	
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", nil)
+	})
+
+	e.POST("/about", func(c echo.Context) error {
+		return c.Render(200, "about", nil)
+	})
+	
+	e.POST("/greeting", func(c echo.Context) error {
+		return c.Render(200, "greeting", nil)
+	})
+
+	e.POST("/htmx", func(c echo.Context) error {
+		htmx.Key = generateProductKey(config)
+		return c.Render(200, "htmx", htmx)
+	})
+	
+	e.Logger.Fatal(e.Start(":42069"))
+}
